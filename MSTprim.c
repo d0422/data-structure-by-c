826d10469f5bbbf59c72b471dcf5d27887e83bd7
d0422 <rlfehd2013@naver.com>
2022-06-13 23:27:36 +0900

    MSTprim

--- MSTprim.c
+++ MSTprim.c
@@ -1,28 +1,38 @@
-// 미완
+//근데 이제 재귀를 곁들인...
 #include <stdio.h>
 #include <stdlib.h>
 #define vertices 10
+#define MAX 999999
+int result = 0;
+int selected[vertices] = {
+    0,
+};
+int dist[vertices] = {
+    MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX};
 struct node
 {
     struct node *next;
-    int data;
+    int dest;
+    int cost;
 };
 struct graph
 {
     struct node *v[vertices];
 };
-void insert_edge(struct graph *g, int x, int y)
+void insert_edge(struct graph *g, int x, int y, int cost)
 {
     struct node *newnode1 = (struct node *)malloc(sizeof(struct node));
-    newnode1->data = y;
+    newnode1->dest = y;
+    newnode1->cost = cost;
     newnode1->next = NULL;
     newnode1->next = g->v[x]->next;
     g->v[x]->next = newnode1;
 
     struct node *newnode2 = (struct node *)malloc(sizeof(struct node));
-    newnode2->data = x;
+    newnode2->dest = x;
     newnode2->next = NULL;
     newnode2->next = g->v[y]->next;
+    newnode2->cost = cost;
     g->v[y]->next = newnode2;
 }
 void init_graph(struct graph *g)
@@ -34,16 +44,57 @@ void init_graph(struct graph *g)
         g->v[x]->next = NULL;
     }
 }
+int min(int a, int b)
+{
+    if (a >= b)
+        return b;
+    else
+        return a;
+}
+void prim(struct graph *g, int start)
+{
+    struct node *cur;
+    struct node *ck = NULL;
+    int MIN = MAX;
+    int x;
+    int sel = -1;
+    printf("%d ", start);
+    selected[start] = 1;
+    cur = g->v[start]->next;
+    while (cur != NULL)
+    {
+        dist[cur->dest] = min(dist[cur->dest], cur->cost);
+        cur = cur->next;
+    }
+    for (x = 0; x < vertices; x++)
+    {
+        if (MIN > dist[x] && selected[x] == 0)
+        {
+            MIN = dist[x];
+            sel = x;
+        }
+    }
+    if (sel != -1)
+    {
+        result += MIN;
+        prim(g, sel);
+    }
+}
 int main()
 {
     struct graph *g = (struct graph *)malloc(sizeof(struct graph));
-    graph_init(g);
-    insert_edge(g, 1, 2);
-    insert_edge(g, 1, 6);
-    insert_edge(g, 2, 4);
-    insert_edge(g, 1, 3);
-    insert_edge(g, 3, 5);
-    insert_edge(g, 5, 7);
-    insert_edge(g, 8, 7);
-    insert_edge(g, 8, 4);
+    init_graph(g);
+    insert_edge(g, 1, 2, 3);
+    insert_edge(g, 1, 6, 4);
+    insert_edge(g, 2, 4, 2);
+    insert_edge(g, 1, 3, 1);
+    insert_edge(g, 3, 5, 8);
+    insert_edge(g, 5, 7, 5);
+    insert_edge(g, 8, 7, 7);
+    insert_edge(g, 8, 4, 9);
+    insert_edge(g, 1, 8, 2);
+    insert_edge(g, 5, 4, 1);
+    printf("path : ");
+    prim(g, 1);
+    printf("\ncost : %d", result);
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 사장님 나빠요
 
 <img src="https://user-images.githubusercontent.com/99241871/173236880-b2c4c08e-07c8-4984-a749-9458c07b27cf.png" width="500" height="350">
+<img src="https://ih1.redbubble.net/image.736108896.8545/fposter,small,wall_texture,product,750x1000.u2.jpg" width ="600" height="500">
